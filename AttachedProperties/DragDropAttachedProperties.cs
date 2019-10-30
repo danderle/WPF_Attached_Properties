5c6f4aadd5782639ce67d7c7d600aa6e96ec20c2
Anderle <DeAn@igg.local>
2019-10-30 10:23:10 +0100

    the custom attached properties

---
+++ AttachedProperties/DragDropAttachedProperties.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CrypticApp
+{
+    public class Behaviors
+    {
+        public static readonly DependencyProperty DropFileCommandProperty =
+            DependencyProperty.RegisterAttached("DropFileCommand", typeof(ICommand),
+            typeof(Behaviors), new UIPropertyMetadata(
+                default(ICommand),
+                new PropertyChangedCallback(DropFileCommandChanged)
+                ));
+
+        public static void DropFileCommandChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            FrameworkElement element = (FrameworkElement)sender;
+
+            element.Drop += Element_DropFile;
+        }
+
+        private static void Element_DropFile(object sender, DragEventArgs e)
+        {
+            FrameworkElement element = (FrameworkElement)sender;
+
+            ICommand command = GetDropFileCommand(element);
+
+            command.Execute(e);
+        }
+
+        public static void SetDropFileCommand(UIElement element, ICommand value)
+        {
+            element.SetValue(DropFileCommandProperty, value);
+        }
+
+        public static ICommand GetDropFileCommand(UIElement element)
+        {
+            return (ICommand)element.GetValue(DropFileCommandProperty);
+        }
+    }
+    
+}
