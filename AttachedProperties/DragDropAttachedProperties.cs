c0962663a2898b0cc0caa7414da9e69e24e2f186
Anderle <DeAn@igg.local>
2019-11-04 17:17:20 +0100

    Added some comments

--- AttachedProperties/DragDropAttachedProperties.cs
+++ AttachedProperties/DragDropAttachedProperties.cs
@@ -9,15 +9,26 @@ using System.Windows.Input;
 
 namespace CrypticApp
 {
-    public class Behaviors
+    /// <summary>
+    /// Enables dragging and dropping files onto any framework element
+    /// </summary>
+    public class DragDrop
     {
+        /// <summary>
+        /// Registers the new attached property
+        /// </summary>
         public static readonly DependencyProperty DropFileCommandProperty =
             DependencyProperty.RegisterAttached("DropFileCommand", typeof(ICommand),
-            typeof(Behaviors), new UIPropertyMetadata(
+            typeof(DragDrop), new UIPropertyMetadata(
                 default(ICommand),
                 new PropertyChangedCallback(DropFileCommandChanged)
                 ));
 
+        /// <summary>
+        /// Fires when a new command is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public static void DropFileCommandChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
             FrameworkElement element = (FrameworkElement)sender;
@@ -25,11 +36,21 @@ namespace CrypticApp
             element.Drop += Element_DropFile;
         }
 
+        /// <summary>
+        /// handles the preview drag over for textboxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private static void element_PreviewDragOver(object sender, DragEventArgs e)
         {
             e.Handled = true;
         }
 
+        /// <summary>
+        /// Calls the command bound to the attached property (see viewmodel)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private static void Element_DropFile(object sender, DragEventArgs e)
         {
             FrameworkElement element = (FrameworkElement)sender;
@@ -39,17 +60,21 @@ namespace CrypticApp
             command.Execute(e);
         }
 
-        private static void element_PreviewDragEnter(object sender, DragEventArgs e)
-        {
-            e.Handled = true;
-            
-        }
-
+        /// <summary>
+        /// the setter for attached property
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="value"></param>
         public static void SetDropFileCommand(UIElement element, ICommand value)
         {
             element.SetValue(DropFileCommandProperty, value);
         }
 
+        /// <summary>
+        /// The getter for attached property
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
         public static ICommand GetDropFileCommand(UIElement element)
         {
             return (ICommand)element.GetValue(DropFileCommandProperty);
