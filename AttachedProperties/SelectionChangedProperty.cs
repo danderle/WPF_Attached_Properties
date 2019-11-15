b233dda062d9e5cf1658c72fe5af41f599f208c2
Anderle <DeAn@igg.local>
2019-11-15 15:53:58 +0100

    Attached Property that executes an ICommand when the selection of a ComboBox is changed

---
+++ AttachedProperties/SelectionChangedProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CrypticApp
+{
+    /// <summary>
+    /// Calls a command method when the selection of a combobox is changed
+    /// </summary>
+    public class SelectionChangedProperty : BaseAttachedProperty<SelectionChangedProperty, ICommand>
+    {
+        /// <summary>
+        /// Fires when a new command is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            comboBox.SelectionChanged += (ss, ee) =>
+            {
+                ICommand command = GetValue(comboBox);
+
+                command.Execute(e);
+            };
+        }
+    }
+    
+}
